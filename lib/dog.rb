f7d2d78d68980d204f41938eb6bcce37d64654ba
Michael Thomas <michaelthomas0611@gmail.com>
2020-05-22 04:12:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,9 @@
 class Dog
-  def name=(dog_name)
+  def name=(dog_name) #Setter properties are written like that " property= "
     this_dogs_name = dog_name
   end
 
-  def name
+  def name #Getter property
     this_dogs_name
   end
 end
