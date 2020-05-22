18c6a0fe493a5b3518ae9689b7c04a2ceafcb927
Michael Thomas <michaelthomas0611@gmail.com>
2020-05-22 04:18:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,23 @@
 class Dog
-  def name=(dog_name) #Setter properties are written like that " property= "
-    this_dogs_name = dog_name
+  def name=(dog_name) #Setter properties are 
+                      # written like that " property= "
+    @this_dogs_name = dog_name
   end
 
   def name #Getter property
-    this_dogs_name
+    @this_dogs_name
   end
 end
 
 lassie = Dog.new
 lassie.name= "Lassie"
+
+puts lassie.name
+
+# define an instance variable by 
+# prefacing the variable name with 
+# an @ symbol.
+
+# the value held by an instance variable 
+# is specific to whatever instance of the 
+# class it happens to belong to. 
